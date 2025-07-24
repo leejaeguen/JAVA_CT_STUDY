169532824f004554c21ee1e7140a2f655ef7034f
김민지 <ghi512151@gmail.com>
2025-07-24 22:19:48 +0900

    week1 pgs_콜라문제 주석 추가

--- 김민지/1주차/pgs_콜라문제.java
+++ 김민지/1주차/pgs_콜라문제.java
@@ -1,3 +1,8 @@
+/**
+ * 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/132267?language=java
+ * 풀이 날짜: 2025.07.24
+ */
+
 class Solution {
     public int solution(int a, int b, int n) {
         int totalCoke = 0;
