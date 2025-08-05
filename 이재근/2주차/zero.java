4e4b5565cdc74217f9294967ad21afa46ada7544
leejaeguen <worms0421@gmail.com>
2025-08-05 18:21:47 +0900

    2주차_이재근

---
+++ 이재근/2주차/zero.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int K = sc.nextInt();
+        Stack<Integer> stack = new Stack<>();
+        
+        for (int i = 0; i < K; i++) {
+            int num = sc.nextInt();
+            if (num == 0) {
+                stack.pop();
+            } else {
+                stack.push(num);
+            }
+        }
+        int sum = 0;
+        for (int n : stack) {
+            sum += n;
+        }
+
+        System.out.println(sum);
+    }
+}
